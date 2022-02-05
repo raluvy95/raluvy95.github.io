76cc3fd7c63a031b58b35aaacdd20aeb3c7479e9
CatNowBlue <43247023+raluvy95@users.noreply.github.com>
2022-02-05 20:08:21 +0200

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,77 @@
+const data = {
+    mostActive: [
+        {
+            name: "YouTube",
+            username: "CatNowBlue",
+            description: "This is where CatNowBlue was born.",
+            url: "https://youtube.com/catnowblue"
+        },
+        {
+            name: "GitHub",
+            username: "raluvy95",
+            description: "The site where I share my source code!",
+            url: "https://github.com/raluvy95"
+        },
+        {
+            name: "Discord",
+            username: "catnowblue",
+            description: "The most common way to communicate with our community",
+            url: "https://discord.gg/3CVqX5bphG"
+        },
+        {
+            name: "Instagram",
+            username: "catnowblue.ro",
+            description: "Another way to communicate with me, but not often.",
+            url: "https://instagram.com/catnowblue.ro"
+        },
+        {
+            name: "Reddit",
+            username: "therealraluvy95",
+            description: "Where I collect memes and maybe I will post there",
+            url: "https://reddit.com/therealraluvy95"
+        }
+    ],
+    leastActive: [
+        {
+            name: "Twitter",
+            username: "catnowblue",
+            description: "The better alternative to Facebook, but recenty... the site went shit and I don't use it anymore.",
+            url: "https://twitter.com/catnowblue"
+        },
+        {
+            name: "Steam",
+            username: "CatNowBlue",
+            description: "Weird way to communicate with me, but I only use it to download and run games",
+            url: "https://steamcommunity.com/id/CatNowBlue"
+        },
+        {
+            name: "Twitch",
+            username: "catnowblue",
+            description: "Not interesed in streaming yet...",
+            url: "https://twitch.tv/catnowblue"
+        }
+    ]
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    for(const listactive of data.mostActive) {
+        document.getElementById('active').innerHTML += `<li class="item" URL=${listactive.url} onclick="clicker(this);">
+        <h2>${listactive.name}</h2>
+        <p><span class="username">Username:</span> ${listactive.username}</p>
+        <p>${listactive.description}</p>
+        </li>`
+    }
+    for(const listactive of data.leastActive) {
+        document.getElementById('inactive').innerHTML += `<li class="item" URL=${listactive.url} onclick="clicker(this);">
+        <h2>${listactive.name}</h2>
+        <p><span class="username">Username:</span> ${listactive.username}</p>
+        <p>${listactive.description}</p>
+        </li>`
+    }
+})
+
+function clicker(el) {
+    const URL = el.getAttribute("url")
+    window.open(URL, "_blank")
+}
