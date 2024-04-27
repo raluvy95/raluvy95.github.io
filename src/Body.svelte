6babda72691a2dd7b4374bfd872586a99f372919
nrdsstuff <nrdfoss@gmail.com>
2024-04-27 17:52:07 +0200

    fixed mobile website

--- src/Body.svelte
+++ src/Body.svelte
@@ -18,67 +18,63 @@
     }
 </script>
 
-<div class="main">
-    <article>
-        <div class="output">
-            <div class="welcomeMessage">
-                Welcome to https://raluvy95.github.io!<br />
-                This is CatNowBlue's personal website.<br />
-                By the way, this page is
-                <a
-                    href="https://github.com/raluvy95/raluvy95.github.io"
-                    target="_blank">open source</a
-                >
+<article>
+    <div class="output">
+        <div class="welcomeMessage">
+            Welcome to https://raluvy95.github.io!<br />
+            This is CatNowBlue's personal website.<br />
+            By the way, this page is
+            <a
+                href="https://github.com/raluvy95/raluvy95.github.io"
+                target="_blank">open source</a
+            >
+        </div>
+    </div>
+    <div class="prompt">
+        <Prompt>
+            catfetch <a class="egg" href="egg.png" target="_blank"> --meow </a>
+        </Prompt>
+    </div>
+    <div class="output">
+        <div class="neofetch">
+            <div class="kitty">
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+                <pre on:click={changeCat}>{cats[cats.length - 1]}</pre>
+            </div>
+            <div class="info">
+                󰄛 CatNowblue, cutecat, CNB<br />
+                󰊜 she/her<br />
+                 Europe, Romania<br />
+                 {myAge}<br />
+                <hr />
+                 ASUS TUF Gaming F15<br />
+                 i7-11800H (16) @ 4.6GHz<br />
+                󰍛 16 GB RAM<br />
+                󰍛 NVIDIA GeForce RTX 3050 Ti<br />
+                 {timeFormat(seconds)}<br />
+                <hr />
+                 NixOS<br />
+                 6.1<br />
+                 zsh<br />
+                 GNOME<br />
+                <hr />
+                <a class="egg" href="egg.png" target="_blank">󰪯 egg</a>
             </div>
         </div>
-        <div class="prompt">
-            <Prompt>
-                catfetch <a class="egg" href="egg.png" target="_blank">
-                    --meow
-                </a>
-            </Prompt>
+    </div>
+    <Prompt>cat links.txt</Prompt>
+    <div class="output">
+        <div class="links">
+            <a href="https://youtube.com/@catnowblue"> YouTube</a>
+            <a href="https://github.com/raluvy95"> Github</a>
+            <a href="https://instagram.com/catnowblue.ro"> Instagram</a>
+            <a href="https://techhub.social/@catnowblue">󰫑 Mastodon</a>
+            <a href="https://discord.gg/BK57NZ8vTX">󰙯 Discord</a>
         </div>
-        <div class="output">
-            <div class="neofetch">
-                <div class="kitty">
-                    <!-- svelte-ignore a11y-click-events-have-key-events -->
-                    <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-                    <pre on:click={changeCat}>{cats[cats.length - 1]}</pre>
-                </div>
-                <div class="info">
-                    󰄛 CatNowblue, cutecat, CNB<br />
-                    󰊜 she/her<br />
-                     Europe, Romania<br />
-                     {myAge}<br />
-                    <hr />
-                     ASUS TUF Gaming F15<br />
-                     i7-11800H (16) @ 4.6GHz<br />
-                    󰍛 16 GB RAM<br />
-                    󰍛 NVIDIA GeForce RTX 3050 Ti<br />
-                     {timeFormat(seconds)}<br />
-                    <hr />
-                     NixOS<br />
-                     6.1<br />
-                     zsh<br />
-                     GNOME<br />
-                    <hr />
-                    <a class="egg" href="egg.png" target="_blank">󰪯 egg</a>
-                </div>
-            </div>
-        </div>
-        <Prompt>cat links.txt</Prompt>
-        <div class="output">
-            <div class="links">
-                <a href="https://youtube.com/@catnowblue"> YouTube</a>
-                <a href="https://github.com/raluvy95"> Github</a>
-                <a href="https://instagram.com/catnowblue.ro"> Instagram</a>
-                <a href="https://techhub.social/@catnowblue">󰫑 Mastodon</a>
-                <a href="https://discord.gg/BK57NZ8vTX">󰙯 Discord</a>
-            </div>
-        </div>
-        <Prompt>dd if=/dev/zero of=/dev/sda1<Cursor /></Prompt>
-    </article>
-</div>
+    </div>
+    <Prompt>dd if=/dev/zero of=/dev/sda1<Cursor /></Prompt>
+</article>
 
 <style lang="scss">
     .egg {
@@ -100,10 +96,13 @@
         display: flex;
         flex-direction: column;
         gap: 1rem;
-        height: 100%;
-        flex-grow: 1;  
         overflow: scroll;
-        height: calc(100vh - 55px);
+        flex-grow: 1;
+        position: fixed;
+        top: 8px;
+        bottom: 8px;
+        left: 8px;
+        right: 8px;
     }
 
     .neofetch {
@@ -123,14 +122,6 @@
         justify-content: center;
         align-items: center;
         user-select: none;
-        // letter-spacing: -4px;
-        // line-height: 1;
-        // word-spacing: -9px;
-        // .nFetchImage {
-        //     margin: 0px;
-        //     line-height: 1.3;
-        //     text-decoration: none;
-        // }
     }
 
     .links {
