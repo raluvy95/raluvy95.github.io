9114896a7df8c6b94d4fbe5fba709d0ed6d26613
raluvy95 <ralucadoge069@gmail.com>
2024-04-27 15:20:58 +0300

    improvements
    Co-authored-by: NRD <NRDsstuff@users.noreply.github.com>

--- src/Body.svelte
+++ src/Body.svelte
@@ -1,95 +1,108 @@
 <script lang="ts">
     import { yearsOld, timeFormat } from "./time";
     import { cats } from "./catascii";
-    import Cursor from "./Cursor.svelte";
+    import Cursor from "./lib/Cursor.svelte";
+    import Prompt from "./lib/Prompt.svelte";
     let seconds: number = 0;
 
     const myAge = yearsOld(Date.now(), Date.parse("18 Jan 2005 00:00:00 GMT"));
-    const randomCat = cats[Math.floor(Math.random() * cats.length)];
+    let randomCat = cats[Math.floor(Math.random() * cats.length)];
     setInterval(() => {
         seconds++;
     }, 1000);
 
     const prompt = "cnb@kitty$ ";
+
+    function changeCat() {
+        randomCat = cats[Math.floor(Math.random() * cats.length)];
+    }
 </script>
 
 <div class="main">
     <article>
-        <div class="welcomeMessage">
-            Welcome to https://raluvy95.github.io!<br />
-            This is CatNowBlue's personal website.<br />
-            By the way, this page is
-            <a
-                href="https://github.com/raluvy95/raluvy95.github.io"
-                target="_blank">open source</a
-            >
-        </div>
-        <div class="prompt">
-            {prompt}catfetch
-            <a class="egg" href="https://i.imgur.com/ArXAMiN.png">--meow</a>
-        </div>
-        <div class="neofetch">
-            <div class="kitty">
-                <a class="nFetchImage" href="https://i.imgur.com/A1gZqtK.png"
-                    >{randomCat}</a
+        <div class="output">
+            <div class="welcomeMessage">
+                Welcome to https://raluvy95.github.io!<br />
+                This is CatNowBlue's personal website.<br />
+                By the way, this page is
+                <a
+                    href="https://github.com/raluvy95/raluvy95.github.io"
+                    target="_blank">open source</a
                 >
             </div>
-            <div class="info">
-                󰄛 CatNowblue, cutecat, CNB<br />
-                󰊜 she/her<br />
-                 Europe, Romania<br />
-                 {myAge}<br />
-                <hr />
-                 ASUS TUF Gaming F15<br />
-                 i7-11800H (16) @ 4.6GHz<br />
-                󰍛 16 GB RAM<br />
-                󰍛 NVIDIA GeForce RTX 3050 Ti<br />
-                 {timeFormat(seconds)}<br />
-                <hr />
-                 NixOS<br />
-                 6.1<br />
-                 zsh<br />
-                 GNOME<br />
-                <hr />
-                <a class="egg" href="https://i.imgur.com/9N26ALo.png">󰪯 egg</a>
-            </div>
-        </div>
-        <div class="prompt">{prompt}cat links.txt</div>
-        <div class="links">
-            <a href="https://youtube.com/@catnowblue"> Youtube</a>
-            <a href="https://github.com/raluvy95"> Github</a>
-            <a href="https://instagram.com/catnowblue.ro"> Instagram</a>
-            <a href="https://techhub.social/@catnowblue">󰫑 Mastodon</a>
-            <a href="https://discord.gg/BK57NZ8vTX">󰙯 Discord</a>
         </div>
         <div class="prompt">
-            {prompt}dd if=/dev/zero of=/dev/sda1<Cursor />
+            <Prompt>
+                catfetch <a class="egg" href="egg.png" target="_blank">
+                    --meow
+                </a>
+            </Prompt>
         </div>
+        <div class="output">
+            <div class="neofetch">
+                <div class="kitty">
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+                    <pre on:click={changeCat}>{cats[cats.length - 1]}</pre>
+                </div>
+                <div class="info">
+                    󰄛 CatNowblue, cutecat, CNB<br />
+                    󰊜 she/her<br />
+                     Europe, Romania<br />
+                     {myAge}<br />
+                    <hr />
+                     ASUS TUF Gaming F15<br />
+                     i7-11800H (16) @ 4.6GHz<br />
+                    󰍛 16 GB RAM<br />
+                    󰍛 NVIDIA GeForce RTX 3050 Ti<br />
+                     {timeFormat(seconds)}<br />
+                    <hr />
+                     NixOS<br />
+                     6.1<br />
+                     zsh<br />
+                     GNOME<br />
+                    <hr />
+                    <a class="egg" href="egg.png" target="_blank">󰪯 egg</a>
+                </div>
+            </div>
+        </div>
+        <Prompt>cat links.txt</Prompt>
+        <div class="output">
+            <div class="links">
+                <a href="https://youtube.com/@catnowblue"> YouTube</a>
+                <a href="https://github.com/raluvy95"> Github</a>
+                <a href="https://instagram.com/catnowblue.ro"> Instagram</a>
+                <a href="https://techhub.social/@catnowblue">󰫑 Mastodon</a>
+                <a href="https://discord.gg/BK57NZ8vTX">󰙯 Discord</a>
+            </div>
+        </div>
+        <Prompt>dd if=/dev/zero of=/dev/sda1<Cursor /></Prompt>
     </article>
 </div>
 
 <style lang="scss">
-    @use "./style/palette.scss" as p;
-
     .egg {
         text-decoration: none;
     }
 
-    .main {
+    .output {
+        padding-left: 1rem;
     }
 
     article {
         border-width: 3px;
         border-style: solid;
-        border-color: p.$foreground;
+        border-color: var(--foreground);
         border-radius: 15px;
         align-items: left;
         padding: 1rem;
+        padding-left: 0;
         display: flex;
         flex-direction: column;
         gap: 1rem;
         height: 100%;
         flex-grow: 1;
+        min-height: calc(100vh - 55px);
     }
 
     .neofetch {
@@ -108,11 +121,15 @@
         display: flex;
         justify-content: center;
         align-items: center;
-        .nFetchImage {
-            margin: 0px;
-            line-height: 1.3;
-            text-decoration: none;
-        }
+        user-select: none;
+        // letter-spacing: -4px;
+        // line-height: 1;
+        // word-spacing: -9px;
+        // .nFetchImage {
+        //     margin: 0px;
+        //     line-height: 1.3;
+        //     text-decoration: none;
+        // }
     }
 
     .links {
@@ -122,14 +139,20 @@
         a {
             width: fit-content;
         }
-    }
-
-    footer {
-        text-align: center;
-        margin: 5px;
+        line-height: 2vh;
     }
 
     hr {
-        color: p.$foreground;
+        color: var(--foreground);
+    }
+
+    @media only screen and (max-width: 492px) {
+        * {
+            font-size: 3vw;
+        }
+
+        .kitty pre {
+            font-size: 5vw;
+        }
     }
 </style>
---
+++ src/lib/Prompt.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    const prompt = "cnb@kitty ";
+</script>
+
+<div class="prompt">
+    <span class="hostname">&nbsp;{prompt} <span class="arrow"></span></span>
+    <span class="directory"> ~ <span class="arrow"></span> </span>
+    <span class="command"><slot></slot></span>
+</div>
+
+<!-- what svg, we have  kinda cool isn't it? -->
+
+<style lang="scss">
+    .prompt {
+        display: flex;
+    }
+
+    .hostname {
+        color: var(--background);
+        background-color: var(--foreground);
+
+        .arrow {
+            color: var(--foreground);
+            background-color: var(--secondary); // good enough
+        }
+    }
+
+    .directory {
+        color: var(--background);
+        background-color: var(--secondary);
+        padding-left: 0.6rem;
+        .arrow {
+            color: var(--secondary);
+            background-color: var(--background);
+        }
+    }
+
+    .command {
+        padding-left: 0.6rem;
+        color: var(--foreground);
+        background-color: var(--background);
+    }
+</style>
